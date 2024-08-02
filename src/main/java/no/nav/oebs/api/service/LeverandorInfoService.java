2152001ec6004bf402d70b76af277cc04bdda735
bougrNAV <abid.bougroug@nav.no>
2024-08-02 18:52:30 +0200

    versjon 0.0.2

---
+++ src/main/java/no/nav/oebs/api/service/LeverandorInfoService.java
@@ -0,0 +1,56 @@
+package no.nav.oebs.api.service;
+
+import java.time.LocalDate;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.oebs.api.common.utils.ObjektMaps;
+import no.nav.oebs.api.common.model.LevRequest;
+import no.nav.oebs.api.db.repository.PlsqlProcedureRepository;
+import no.nav.oebs.api.db.repository.PlsqlProcedureResult;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Slf4j
+@Service
+@Transactional(noRollbackFor = { Exception.class })
+public class LeverandorInfoService extends ObjektMaps {
+	private static final String PLSQL_PROCEDURE = "xxrtv_mainmanager_api.xxrtv_leverandoerinfo_api";
+
+	private final PlsqlProcedureRepository plsqlProcedureRepository;
+
+	public LeverandorInfoService(PlsqlProcedureRepository plsqlProcedureRepository, ObjectMapper objectMapper) {
+		super(objectMapper);
+		this.plsqlProcedureRepository = plsqlProcedureRepository;
+	}
+
+	public String finnLeverandorTransaksjoner(Integer org_id, String leverandornavn,
+											  String leverandornummer, String leverandorsted, LocalDate lastupdatedate) {
+
+		PlsqlProcedureResult result = executePlsqlProcedure(buildRequest(org_id, leverandornavn,
+				leverandornummer, leverandorsted, lastupdatedate));
+		 if (result.getMessageNumber() < 0) {
+			 throwPlsqlException(result);
+		}
+
+		return result.getData();
+	}
+
+	private LevRequest buildRequest(Integer org_id, String leverandornavn,
+												String leverandornummer, String leverandorsted, LocalDate lastupdatedate) {
+		return LevRequest.builder()
+				.org_id(org_id) //
+				.leverandornavn(leverandornavn) //
+				.leverandornummer(leverandornummer) //
+				.leverandorsted(leverandorsted) //
+				.lastupdatedate(lastupdatedate) //
+				.build();
+	}
+
+	/**
+	 * Kaller PL/SQL-prosedyren som utfører forretningslogikken til operasjonen.
+     */
+	private PlsqlProcedureResult executePlsqlProcedure(LevRequest request) {
+
+		return plsqlProcedureRepository.executeInOutProcedure(PLSQL_PROCEDURE, toJson(request));
+	}
+}
