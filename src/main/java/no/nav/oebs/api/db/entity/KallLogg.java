ff8b5b9d378f134a4bfa7e608a46da6c8dcbae1a
Rasmus Vedholm Krog <113107378+rasmusvknav@users.noreply.github.com>
2026-06-19 10:26:06 +0200

    chore(dedicated-db-user): added schema tag to log table and sequence (#40)

--- src/main/java/no/nav/oebs/api/db/entity/KallLogg.java
+++ src/main/java/no/nav/oebs/api/db/entity/KallLogg.java
@@ -25,7 +25,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@Table(name = "XXRTV_MAINMANAGER_LOGG")
+@Table(name = "XXRTV_MAINMANAGER_LOGG", schema = "XXRTV")
 public class KallLogg {
 
 	public static final String RETNING_INN = "INN";
@@ -35,7 +35,7 @@ public class KallLogg {
 	public static final String TYPE_REST = "REST";
 	public static final String METHOD_POST = "POST";
 	@Id
-	@SequenceGenerator(name = "XXRTV_MAINMANAGER_SEQ", sequenceName = "XXRTV_MAINMANAGER_SEQ", allocationSize = 1)
+	@SequenceGenerator(name = "XXRTV_MAINMANAGER_SEQ", sequenceName = "XXRTV_MAINMANAGER_SEQ", schema = "APPS", allocationSize = 1)
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "XXRTV_MAINMANAGER_SEQ")
 	@Column(name = "KALL_LOGG_ID")
 	private Long id;
